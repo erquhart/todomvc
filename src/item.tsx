609009f522562f0415a9a6a1652e7a79b177b7c8
Shawn Erquhart <shawn@erquh.art>
2024-04-11 15:28:41 -0400

    step 6: add optimistic updates

--- src/item.tsx
+++ src/item.tsx
@@ -5,13 +5,67 @@ import { Input } from "./input";
 
 import { useMutation } from "convex/react";
 import { api } from "../convex/_generated/api";
+import { OptimisticUpdate } from "convex/browser";
+import { Id } from "../convex/_generated/dataModel";
+
+const toggleItemOptimisticUpdate: OptimisticUpdate<{ id: Id<"todos"> }> = (
+  localStore,
+  args,
+) => {
+  const currentValue = localStore.getQuery(api.todo.listItems);
+  if (currentValue !== undefined) {
+    localStore.setQuery(
+      api.todo.listItems,
+      {},
+      currentValue.map((todo) =>
+        todo._id === args.id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  }
+};
+
+const removeItemOptimisticUpdate: OptimisticUpdate<{ id: Id<"todos"> }> = (
+  localStore,
+  args,
+) => {
+  const currentValue = localStore.getQuery(api.todo.listItems);
+  if (currentValue !== undefined) {
+    localStore.setQuery(
+      api.todo.listItems,
+      {},
+      currentValue.filter((todo) => todo._id !== args.id),
+    );
+  }
+};
+
+const updateItemOptimisticUpdate: OptimisticUpdate<{
+  id: Id<"todos">;
+  title: string;
+}> = (localStore, args) => {
+  const currentValue = localStore.getQuery(api.todo.listItems);
+  if (currentValue !== undefined) {
+    localStore.setQuery(
+      api.todo.listItems,
+      {},
+      currentValue.map((todo) =>
+        todo._id === args.id ? { ...todo, title: args.title } : todo,
+      ),
+    );
+  }
+};
 
 export const Item = memo(function Item({ todo }: { todo: any }) {
   const [isWritable, setIsWritable] = useState(false);
 
-  const toggleItem = useMutation(api.todo.toggleItem);
-  const removeItem = useMutation(api.todo.removeItem);
-  const updateItem = useMutation(api.todo.updateItem);
+  const toggleItem = useMutation(api.todo.toggleItem).withOptimisticUpdate(
+    toggleItemOptimisticUpdate,
+  );
+  const removeItem = useMutation(api.todo.removeItem).withOptimisticUpdate(
+    removeItemOptimisticUpdate,
+  );
+  const updateItem = useMutation(api.todo.updateItem).withOptimisticUpdate(
+    updateItemOptimisticUpdate,
+  );
 
   const handleDoubleClick = useCallback(() => {
     setIsWritable(true);
