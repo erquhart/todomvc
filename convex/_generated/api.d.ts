d46dd59a6946cd085e08873216f1120a72fcfb83
Shawn Erquhart <shawn@erquh.art>
2024-04-15 15:16:34 -0400

    step 8: add ai

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as ai from "../ai.js";
 import type * as todo from "../todo.js";
 
 /**
@@ -25,6 +26,7 @@ import type * as todo from "../todo.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  ai: typeof ai;
   todo: typeof todo;
 }>;
 export declare const api: FilterApi<
