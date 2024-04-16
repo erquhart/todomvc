13f1cbb0eba1a5009236f69a47f309feff346a17
Shawn Erquhart <shawn@erquh.art>
2024-04-15 23:08:15 -0400

    step 11: support multiplayer

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,12 +5,18 @@ export default defineSchema({
   todos: defineTable({
     title: v.string(),
     completed: v.boolean(),
-    userId: v.id("users"),
+    listId: v.id("lists"),
   })
-    .index("by_userId", ["userId"])
-    .index("by_userId_completed", ["userId", "completed"]),
+    .index("by_listId", ["listId"])
+    .index("by_listId_completed", ["listId", "completed"]),
   users: defineTable({
     clerkId: v.string(),
-    backgroundImageStorageId: v.optional(v.id("_storage")),
   }).index("by_clerkId", ["clerkId"]),
+  lists: defineTable({
+    shareId: v.string(),
+    userId: v.id("users"),
+    backgroundImageStorageId: v.optional(v.id("_storage")),
+  })
+    .index("by_userId", ["userId"])
+    .index("by_shareId", ["shareId"]),
 });
